28b4583cd6d0f5dbf9e2df6aafd06fe194a6d7bf
loongtao.zhang <loongtao.zhang@outlook.com>
2026-06-12 17:37:27 +0800

    feat: bundle idl-language-server into intellij plugin distribution

--- intellij-extension/src/main/kotlin/com/xidl/idl/lsp/IdlLspServerSupportProvider.kt
+++ intellij-extension/src/main/kotlin/com/xidl/idl/lsp/IdlLspServerSupportProvider.kt
@@ -1,11 +1,15 @@
 package com.xidl.idl.lsp
 
 import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
 import com.xidl.idl.IdlFileType
+import java.nio.file.Files
 
 class IdlLspServerSupportProvider : LspServerSupportProvider {
     override fun fileOpened(
@@ -30,6 +34,23 @@ class IdlLspServerDescriptor(project: Project, presentableName: String) :
         file.fileType == IdlFileType
 
     override fun createCommandLine(): GeneralCommandLine {
+        val pluginId = PluginId.getId("com.xidl.idl")
+        val plugin = PluginManagerCore.getPlugin(pluginId)
+
+        val binName = if (SystemInfo.isWindows) "idl-language-server.exe" else "idl-language-server"
+        val osDir = when {
+            SystemInfo.isWindows -> "windows"
+            SystemInfo.isMac -> "mac"
+            SystemInfo.isLinux -> "linux"
+            else -> "linux"
+        }
+
+        val bundledPath = plugin?.pluginPath?.resolve("bin")?.resolve(osDir)?.resolve(binName)
+
+        if (bundledPath != null && Files.exists(bundledPath)) {
+            return GeneralCommandLine(bundledPath.toString())
+        }
+
         return GeneralCommandLine("idl-language-server")
     }
 }
